83bc3a21854d6d97e633002829097d496dae13fe
ananya9177 <151428838+ananya9177@users.noreply.github.com>
2023-12-21 23:11:54 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,50 +1,41 @@
 const todoList = require("../todo");
 
-const {
-  all,
-  markAsComplete,
-  add,
-  overdue,
-  dueToday,
-  dueLater,
-} = todoList();
-
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 describe("Todo List Test Suite", () => {
-  beforeEach(() => {
-    const currentDate = new Date();
-    const oneDayInMilliseconds = 60 * 60 * 24 * 1000;
-
-    const todos = [
+  beforeAll(() => {
+    const todays_ = new Date();
+    const days_ = 60 * 60 * 24 * 1000;
+    [
       {
         title: "do pupil 4th level",
         completed: false,
-        dueDate: new Date(currentDate.getTime() - 4 * oneDayInMilliseconds).toLocaleDateString("en-CA"),
+        dueDate: new Date(todays_.getTime() - 4 * days_).toLocaleDateString("en-CA"),
       },
       {
-        title: "make christmas presents",
+        title: "oack presents",
         completed: false,
-        dueDate: new Date(currentDate.getTime() - 3 * oneDayInMilliseconds).toLocaleDateString("en-CA"),
+        dueDate: new Date(todays_.getTime() - 3 * days_).toLocaleDateString("en-CA"),
       },
       {
         title: "complete javascript course",
         completed: false,
-        dueDate: currentDate.toLocaleDateString("en-CA"),
+        dueDate: new Date().toLocaleDateString("en-CA"),
       },
       {
         title: "make a website from scratch",
         completed: false,
-        dueDate: new Date(currentDate.getTime() + 3 * oneDayInMilliseconds).toLocaleDateString("en-CA"),
+        dueDate: new Date(todays_.getTime() + 3 * days_).toLocaleDateString(
+          "en-CA"
+        ),
       },
-    ];
-
-    todos.forEach(add);
+      
+    ].forEach(add);
   });
-
   test("Should add a new todo", () => {
     expect(all.length).toEqual(4);
 
     add({
-      title: "A items testcase",
+      title: "A testcase item",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
@@ -52,21 +43,21 @@ describe("Todo List Test Suite", () => {
     expect(all.length).toEqual(5);
   });
 
-  test("Should mark a todo as complete", () => {
+  test("to mark a todo as complete", () => {
     expect(all[1].completed).toEqual(false);
     markAsComplete(1);
     expect(all[1].completed).toEqual(true);
   });
 
-  test("Should retrieve overdue items", () => {
+  test("to retrieve overdue items", () => {
     expect(overdue().length).toEqual(2);
   });
 
-  test("Should retrieve due today items", () => {
+  test("to retrieve due today items", () => {
     expect(dueToday().length).toEqual(2);
   });
 
-  test("Should retrieve due later items", () => {
+  test("to retrieve due later items", () => {
     expect(dueLater().length).toEqual(1);
   });
 });
