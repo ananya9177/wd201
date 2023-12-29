ffba7b3e5edba52e762954688425f2eb6f4c9199
ananya9177 <151428838+ananya9177@users.noreply.github.com>
2023-12-30 01:56:50 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -8,26 +8,19 @@ app.get("/", function (request, response) {
   response.send("Hello World");
 });
 
+// Route to get all todos
 app.get("/todos", async function (_request, response) {
-  console.log("Processing list of all Todos ...");
-  app.get("/todos", async function (_request, response) {
-    try {
-      // Query the database to get a list of all Todos
-      const todos = await Todo.findAll();
-      return response.json(todos);
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-  
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
+  try {
+    // Query the database to get a list of all Todos
+    const todos = await Todo.findAll();
+    return response.json(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
+// Route to get a todo by ID
 app.get("/todos/:id", async function (request, response) {
   try {
     const todo = await Todo.findByPk(request.params.id);
@@ -38,6 +31,7 @@ app.get("/todos/:id", async function (request, response) {
   }
 });
 
+// Route to add a new todo
 app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
@@ -48,6 +42,7 @@ app.post("/todos", async function (request, response) {
   }
 });
 
+// Route to mark a todo as completed
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
@@ -59,33 +54,24 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
   }
 });
 
+// Route to delete a todo by ID
 app.delete("/todos/:id", async function (request, response) {
-  console.log("We have to delete a Todo with ID: ", request.params.id);
-  
-  // FILL IN YOUR CODE HERE
-  app.delete("/todos/:id", async function (request, response) {
-    try {
-      // Find the Todo by ID
-      const todo = await Todo.findByPk(request.params.id);
-  
-      // If the Todo is found, delete it
-      if (todo) {
-        await todo.destroy();
-        return response.json({ success: true, message: "Todo deleted successfully" });
-      } else {
-        // If the Todo with the specified ID is not found
-        return response.status(404).json({ success: false, message: "Todo not found" });
-      }
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-  });
-  
+  try {
+    // Find the Todo by ID
+    const todo = await Todo.findByPk(request.params.id);
 
-  // First, we have to query our database to delete a Todo by ID.
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
+    // If the Todo is found, delete it
+    if (todo) {
+      await todo.destroy();
+      return response.json({ success: true, message: "Todo deleted successfully" });
+    } else {
+      // If the Todo with the specified ID is not found
+      return response.status(404).json({ success: false, message: "Todo not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 });
 
 module.exports = app;
